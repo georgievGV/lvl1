a4b8980e46b18dfd6067cd06405a4b873a0ac08e
GeorgievGV <georgiev_ptg@abv.bg>
2020-07-30 18:30:44 +0300

    30.07

---
+++ FinalExams/Programming Fundamentals Final Exam - 04 April 2020 Group 2/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Regex regex = new Regex(@"^@[#]{1,}([A-Z][A-Za-z\d]{4,}[A-Z])@[#]{1,}$");
+
+            for (int i = 0; i < n; i++)
+            {
+                string barcode = Console.ReadLine();
+
+                if (regex.IsMatch(barcode))
+                {
+                    Regex numReg = new Regex(@"\d+");
+                    Match match = regex.Match(barcode);
+                    if (numReg.IsMatch(match.Value))
+                    {
+                        MatchCollection mc = numReg.Matches(match.Value);
+                        string group = string.Empty;
+                        foreach (Match item in mc)
+                        {
+                            group += item;
+                        }
+                        Console.WriteLine($"Product group: {group}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }   
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+            }
+        }
+    }
+}
